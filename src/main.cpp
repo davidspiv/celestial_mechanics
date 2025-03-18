e6e48620788fde1dcad25bfd42b630aeaa53fa80
davidspiv <daspivack@gmail.com>
2025-03-17 20:31:55 -0700

    working with hardcoded values

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ struct CelestialBody {
   double mass;
 };
 
+const double METERS_PER_AU = 1.496e11;
+const double SECONDS_PER_DAY = 86400;
+
 const double G = 6.67430e-11;      // Gravitational constant
 const double mMercury = 3.3011e23; // [kg]
 const double mVenus = 4.8670e24;
@@ -26,19 +29,51 @@ const double mMars = 6.3900e23;
 //   const double mNeptune = 1.0241e26;
 //   const double mPluto = 1.3030e22;
 
+// ----------------------------------------------------------
+//         Name: mercury
+//  Acc [m/s/s]: X: 0.02089       Y: 0.07184       Z: 0.00395
+//    Vel [m/s]: X: -37177.59990  Y: -10576.72537  Z: -4276.33216
+//     Pos [AU]: X: -0.13008      Y: -0.44729      Z: -0.02460
+// ----------------------------------------------------------
+//         Name: venus
+//  Acc [m/s/s]: X: 0.04460       Y: 0.00203       Z: -0.00255
+//    Vel [m/s]: X: -2220.13867   Y: -49691.54133  Z: -807.21777
+//     Pos [AU]: X: -0.71832      Y: -0.03276      Z: 0.04102
+// ----------------------------------------------------------
+//         Name: earth
+//  Acc [m/s/s]: X: 0.00467       Y: -0.02551      Z: -0.00000
+//    Vel [m/s]: X: 51607.28254   Y: -9452.74623   Z: -0.01688
+//     Pos [AU]: X: -0.17716      Y: 0.96721       Z: 0.00000
+// ----------------------------------------------------------
+//         Name: mars
+//  Acc [m/s/s]: X: -0.01388      Y: 0.00014       Z: 0.00034
+//    Vel [m/s]: X: -551.00146    Y: 52652.44794   Z: 1089.52988
+//     Pos [AU]: X: 1.39059       Y: -0.01384      Z: -0.03450
+
 // [m], [m/s]
-CelestialBody mercury = {
-    "Mercury", {5.7910e10, 0, 0}, {0, 4.7870e4, 0}, mMercury};
-CelestialBody venus = {"Venus", {1.0770e11, 0, 0}, {0, 3.5000e4, 0}, mVenus};
-CelestialBody earth = {"Earth", {1.4959e11, 0, 0}, {0, 2.9780e4, 0}, mEarth};
-CelestialBody mars = {"Mars", {2.2794e11, 0, 0}, {0, 2.4100e4, 0}, mMars};
+// CelestialBody mercury = {
+//     "Mercury", {5.7910e10, 0, 0}, {0, 4.7870e4, 0}, mMercury};
+// CelestialBody venus = {"Venus", {1.0770e11, 0, 0}, {0, 3.5000e4, 0}, mVenus};
+// CelestialBody earth = {"Earth", {1.4959e11, 0, 0}, {0, 2.9780e4, 0}, mEarth};
+
+CelestialBody earth = {
+    "Earth",
+    {-1.756637922977122E-01 * METERS_PER_AU,
+     9.659912850526895E-01 * METERS_PER_AU,
+     2.020629118443605E-04 * METERS_PER_AU},
+    {-1.720762506877444E-02 * METERS_PER_AU / SECONDS_PER_DAY,
+     -3.158782144070574E-03 * METERS_PER_AU / SECONDS_PER_DAY,
+     1.049888593456605E-07 * METERS_PER_AU / SECONDS_PER_DAY},
+    mEarth};
+
+// CelestialBody mars = {"Mars", {2.2794e11, 0, 0}, {0, 2.4100e4, 0}, mMars};
 //   CelestialBody  jupiter = {{7.7857e11, 0, 0}, {0, 1.3070e4, 0}, mJupiter};
 //   CelestialBody  saturn = {{1.4335e12, 0, 0}, {0, 9.6900e3, 0}, mSaturn};
 //   CelestialBody  uranus = {{2.8725e12, 0, 0}, {0, 6.8100e3, 0}, mUranus};
 //   CelestialBody  neptune = {{4.4951e12, 0, 0}, {0, 5.4300e3, 0}, mNeptune};
 //   CelestialBody  pluto = {{5.9064e12, 0, 0}, {0, 4.7400e3, 0}, mPluto};
 
-std::vector<CelestialBody> planets{mercury, venus, earth, mars};
+std::vector<CelestialBody> planets{earth};
 
 size_t scaleValue(double x, size_t currMax, size_t newMax) {
   if (!currMax) {
@@ -105,21 +140,28 @@ int main() {
 
   double secondsPerYear = 31536000;
   int dt = 600; // 10-minute intervals
-  const int steps = secondsPerYear / static_cast<int>(dt);
+  const int steps = secondsPerYear / dt;
 
-  for (int i = 0; i < steps; i++) {
-    std::vector<CelestialBody> updatedBody;
-    for (const auto &p : planets) {
-      updatedBody.push_back(rungeKuttaStep(p, dt));
-    }
-    planets = updatedBody;
+  //   for (int i = 0; i < steps; i++) {
+  //     std::vector<CelestialBody> updatedBody;
+  //     for (const auto &p : planets) {
+  //       updatedBody.push_back(rungeKuttaStep(p, dt));
+  //     }
+  //     planets = updatedBody;
 
-    for (CelestialBody p : planets) {
-      Coord pos = p.pos / 1.496e+11;
-      int x = scaleValue(pos.x, 2, 150) + picWidth / 2;
-      int y = scaleValue(-pos.y, 2, 150) + picHeight / 2;
-      pic.set(y, x, 0, 255, 0);
-    }
+  //     for (CelestialBody p : planets) {
+  //       Coord pos = p.pos / METERS_PER_AU;
+  //       int x = scaleValue(pos.x, 2, 150) + picWidth / 2;
+  //       int y = scaleValue(-pos.y, 2, 150) + picHeight / 2;
+  //       pic.set(x, y, 0, 255, 0);
+  //     }
+  // }
+
+  for (CelestialBody p : planets) {
+    Coord pos = p.pos / 1.496e+11;
+    int x = scaleValue(pos.x, 2, 150) + picWidth / 2;
+    int y = scaleValue(-pos.y, 2, 150) + picHeight / 2;
+    pic.set(x, y, 0, 255, 0);
   }
 
   for (CelestialBody body : planets) {
