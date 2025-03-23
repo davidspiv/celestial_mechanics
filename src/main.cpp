9cf6a5e099feeecddb98954358ef870c9c45d420
davidspiv <daspivack@gmail.com>
2025-03-22 20:48:26 -0700

    organized main

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,33 @@
 
 
 int main() {
-  const double julianDay = getDate();
-  //   const double julianDay = 0;
-  const int dt = 345600; // 1-week intervals
-  const int steps = round(SEC_PER_DAY * julianDay / double(dt));
-
-  const int picSideLength = 500;
-  Picture pic(picSideLength, picSideLength, 0, 0, 0);
-
-  // parallel vectors to represent planets
+  // Parallel vectors to represent planets
   std::vector<OrbitalElements> elements;
   std::vector<CelestialBody> bodies;
 
-  //   populateSolutions(bodies, julianDay);
-  //   const size_t systemSize = 31;
+  // Initialize planets
+  const double julianDay = getDate();
   populatePlanets(elements, bodies);
-  const size_t systemSize = approxSystemSize(elements);
-
   for (size_t i = 0; i < bodies.size() - 1; i++) {
     populateStateVectors(elements[i], bodies[i], 0);
   }
 
+  // Initialize photo
+  const size_t picSize = 500;
+  const size_t systemSizeAU = approxSystemSize(elements);
+  Picture pic(picSize, picSize, 0, 0, 0);
+
+  // Update planets and photo
+  const int dt = SEC_PER_DAY; // 1-week intervals
+  const int steps = round(SEC_PER_DAY * julianDay / double(dt));
   for (int i = 0; i < steps; i++) {
     updateBodies(bodies, dt);
-    drawBodies(bodies, pic, systemSize);
+    drawBodies(bodies, pic, systemSizeAU);
   }
-  drawBodies(bodies, pic, systemSize, true);
 
+  // Handle results
+  drawBodies(bodies, pic, systemSizeAU, true);
   pic.save("result.png");
-
-  printTest(bodies, julianDay);
-  //   printResults(bodies);
+  //   printTest(bodies, julianDay);
+  printResults(bodies);
 }
